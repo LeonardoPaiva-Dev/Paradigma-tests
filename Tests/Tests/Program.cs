270aae3f693f3c4f8fd7bb6af15d9b917115ae5b
Leonardo Paiva <leonardopaivagamedev@gmail.com>
2021-02-16 23:42:18 -0300

    Exercício 1 finalizado

---
+++ Tests/Tests/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 7, 5, 3, 9, 6, 4, 1 };
+            int[] newArray = (int[])array.Clone();
+            
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if(newArray[i] == 9)
+                {
+                    newArray[i] = 5;
+                }
+            }
+
+            newArray = Array.FindAll(newArray, removeQuatro);
+
+            //printando array oiginal
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("\n");
+
+            //printando novo array
+            foreach (int i in newArray)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+
+        static bool removeQuatro(int n)
+        {
+            return n != 4;
+        }
+    }
+}
